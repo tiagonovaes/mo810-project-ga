6a2431c53d64c655f20c000596183d16aa5def52
Vinicius Barboza <viniciusbcomz@gmail.com>
2014-12-03 11:35:35 -0200

    Adicionada funcao para ler atributos do CSV, de personagens especificas ou media dos atributos de um time.

---
+++ src/Atributos.m
@@ -0,0 +1,11 @@
+function X=Atributos(filename, id=0)
+
+% Le um arquivo CSV
+X = dlmread(filename,";");
+% Busca os 6 atributos de cada
+X = X(2:size(X,1),4:9);
+
+% Calcula atributos individuais ou do time de acordo com o 'id'
+if (id > 0)
+      X = mean(X(id,:), 1);
+endif
